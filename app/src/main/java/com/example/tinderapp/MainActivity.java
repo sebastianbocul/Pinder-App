5bd5a3eef3bda896a76836998f7fc04a6f5464f2
sebastianbocul <seba23495@gmail.com>
2020-04-08 16:39:02 +0200

    Fixed card layout

--- app/src/main/java/com/example/tinderapp/MainActivity.java
+++ app/src/main/java/com/example/tinderapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.service.chooser.ChooserTargetService;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,13 +23,18 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
+    private cards cards_data;
+    private arrayAdapter arrayAdapter;
     private int i;
 
     private FirebaseAuth mAuth;
+
+    ListView listView;
+    List<cards> rowItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
 
         checkUserSex();
 
-        al = new ArrayList<>();
-        al.add("php");
+        rowItems = new ArrayList<cards>();
 
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+
+        arrayAdapter = new arrayAdapter(this, R.layout.item,rowItems );
 
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
@@ -52,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
+                rowItems.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -176,7 +182,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if(dataSnapshot.exists()){
-                    al.add(dataSnapshot.child("name").getValue().toString());
+
+                    cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString());
+                    rowItems.add(item);
                     arrayAdapter.notifyDataSetChanged();
                 }
             }
