5597a99ff408767668deccaca502c0e4c3be13ff
sebastianbocul <seba23495@gmail.com>
2020-05-14 19:07:48 +0200

    Created deleteMatches function that delete match info in other users matches

--- app/src/main/java/com/example/tinderapp/SettingsActivity.java
+++ app/src/main/java/com/example/tinderapp/SettingsActivity.java
@@ -9,11 +9,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Credentials;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.tinderapp.Matches.MatchesActivity;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -24,8 +26,11 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
@@ -108,8 +113,9 @@ public class SettingsActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d("del", "User account deleted.");
                             Toast.makeText(SettingsActivity.this,"User account deleted.",Toast.LENGTH_LONG).show();
-                            deleteDatabaseAndStorage();
-                            logoutUser();
+                           // deleteDatabaseAndStorage();
+                            deleteMatches();
+                            //logoutUser();
 
                         } else {
                         Log.w("del","Something is wrong!");
@@ -128,10 +134,41 @@ public class SettingsActivity extends AppCompatActivity {
                     }
                 });
     }
-    private void deleteDatabaseAndStorage(){
-        DatabaseReference mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
-        mUserDatabase.removeValue();
 
+    private void deleteMatches() {
+
+        DatabaseReference users = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        users.child(userId).child("connections").child("matches").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    try {
+                        users.child(ds.getKey()).child("connections").child("matches").child(userId).removeValue();
+                        users.child(ds.getKey()).child("connections").child("yes").child(userId).removeValue();
+                        DatabaseReference mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+                        mUserDatabase.removeValue();
+
+                        deleteDatabaseAndStorage();
+
+                    }catch (Exception e){
+                        Toast.makeText(SettingsActivity.this,"Oooops something went wrong",Toast.LENGTH_SHORT).show();
+                    }
+                    
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void deleteDatabaseAndStorage(){
 
         filePath = FirebaseStorage.getInstance().getReference().child("images").child(userId);
         StorageReference storageRef = filePath;
@@ -144,6 +181,7 @@ public class SettingsActivity extends AppCompatActivity {
                             // All the items under listRef.
                             item.delete();
                         }
+                        logoutUser();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
