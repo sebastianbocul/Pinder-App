a002028ab1f65196da85e49159499a263ae1859b
sebastianbocul <seba23495@gmail.com>
2020-05-22 13:27:38 +0200

    Fixed duplicating cards bug

--- app/src/main/java/com/example/tinderapp/MainActivity.java
+++ app/src/main/java/com/example/tinderapp/MainActivity.java
@@ -239,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,UsersProfilesActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 intent.putExtra("userId", userId);
-
+                Log.d("mainActivity","Number of cards : " + rowItems);
                 ArrayList<String> stringList = new ArrayList(objec.getMutualTagsMap().keySet());
                 Log.d("main",stringList.toString());
                 intent.putStringArrayListExtra("mutualTags",  stringList);
@@ -619,8 +619,9 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         updateMyLoc();
         fillTagsAdapter();
-        getUsersFromDb();
-
+        if(rowItems.size()==0){
+            getUsersFromDb();
+        }
     }
     private void updateMyLoc(){
         DatabaseReference ds = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUID).child("location");
