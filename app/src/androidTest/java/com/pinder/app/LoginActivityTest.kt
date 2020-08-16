8479f38370d8bef4ebfcbf17578b02db8452bbd9
sebastianbocul <seba23495@gmail.com>
2020-08-16 16:57:29 +0200

    Created first UI tests to check elements visibility in LoginActivity

--- app/src/androidTest/java/com/pinder/app/LoginActivityTest.kt
+++ app/src/androidTest/java/com/pinder/app/LoginActivityTest.kt
@@ -2,51 +2,37 @@ package com.pinder.app
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
-
-
 @RunWith(AndroidJUnit4ClassRunner::class)
-class LoginActivityTest{
-    @Test
-    fun test_login_activity() {
+class LoginActivityTest {
 
+    @Test
+    fun test_isActivityInView() {
         val activityScenario = ActivityScenario.launch(LoginActivity::class.java)
 
-        onView(withId(R.id.register)).perform(click())
+        onView(withId(R.id.loginActivity)).check(matches(isDisplayed()))
 
-        onView(withId(R.id.name)).check(matches(isDisplayed()))
     }
 
-    /**
-     * Test both ways to navigate from SecondaryActivity to MainActivity
-     */
-//    @Test
-//    fun test_backPress_toMainActivity() {
-//
-//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-//
-//        onView(withId(R.id.button_next_activity)).perform(click())
-//
-//        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
-//
-//        onView(withId(R.id.button_back)).perform(click()) // method 1
-//
-//        onView(withId(R.id.main)).check(matches(isDisplayed()))
-//
-//        onView(withId(R.id.button_next_activity)).perform(click())
-//
-//        pressBack() // method 2
-//
-//        onView(withId(R.id.main)).check(matches(isDisplayed()))
-//    }
+    @Test
+    fun test_visibility_elements() {
+        val activityScenario = ActivityScenario.launch(LoginActivity::class.java)
+        onView(withId(R.id.regulationsTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.emailTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.email)).check(matches(isDisplayed()))
+        onView(withId(R.id.passwordTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.password)).check(matches(isDisplayed()))
+        onView(withId(R.id.login)).check(matches(isDisplayed()))
+        onView(withId(R.id.registerTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.login_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.sign_in_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.smallLogo)).check(matches(isDisplayed()))
+        onView(withId(R.id.bigLogo)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
+    }
 }
