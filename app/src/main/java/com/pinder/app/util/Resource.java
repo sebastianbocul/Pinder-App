395a7a69fde59c1130e2cfd0e2b3b16da0bea3c0
sebastianbocul <seba23495@gmail.com>
2020-08-27 13:31:03 +0200

    Added Resource class

---
+++ app/src/main/java/com/pinder/app/util/Resource.java
@@ -0,0 +1,37 @@
+package com.pinder.app.util;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class Resource<T> {
+    @NonNull
+    public final Status status;
+    @Nullable
+    public final T data;
+    @Nullable
+    public final String message;
+
+    public Resource(@NonNull Status status, @Nullable T data, @Nullable String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public static <T> Resource<T> authenticated(@Nullable T data) {
+        return new Resource<>(Status.AUTHENTICATED, data, null);
+    }
+
+    public static <T> Resource<T> error(@NonNull String msg, @Nullable T data) {
+        return new Resource<>(Status.ERROR, data, msg);
+    }
+
+    public static <T> Resource<T> loading(@Nullable T data) {
+        return new Resource<>(Status.LOADING, data, null);
+    }
+
+    public static <T> Resource<T> logout() {
+        return new Resource<>(Status.NOT_AUTHENTICATED, null, null);
+    }
+
+    public enum Status {AUTHENTICATED, ERROR, LOADING, NOT_AUTHENTICATED}
+}
