98492530298c6cebd04ba8c76f7d91d7abb38e3e
sebastianbocul <seba23495@gmail.com>
2021-01-16 12:13:07 +0100

    Added navigation component, commented errors

---
+++ app/src/main/java/com/pinder/app/di/CommunicationModules.java
@@ -0,0 +1,24 @@
+package com.pinder.app.di;
+
+import android.content.Context;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ApplicationComponent;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+@Module
+@InstallIn(ApplicationComponent.class)
+class AppModules {
+
+    @Singleton
+    @Provides
+    public static Context application(@ApplicationContext Context context) {
+        return context;
+    }
+
+}
+
+
